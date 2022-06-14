c9180ea2b5d9d4ca92c019237d0bd18d1724b587
Rajesh <rajeshceg3@gmail.com>
2022-06-14 11:31:02 +0000

    Updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('config')
 const port = process.env.PORT || config.get("port")
 
 const server = app.listen(port,(req,res)  =>{
+    res.write(`Server listening for requests on ${port}`);
     console.log(`Server listening for requests on ${port}`)
 })
 
