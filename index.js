1518e8935087ed1523e6f2f5fc9f94d50093a524
Rajesh <rajeshceg3@gmail.com>
2022-06-14 15:26:17 +0530

    Index js

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')()
+const config = require('config')
+const port = process.env.PORT || config.get("port")
+
+const server = app.listen(port,()  =>{
+    console.log(`Server listening for requests on ${port}`)
+})
+
+module.exports = server
+
